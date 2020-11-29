95fce2d64d86ae32144350465a9355de2843b939
yuizho <1918628+yuizho@users.noreply.github.com>
2020-11-29 18:58:25 +0900

    introduce redux

--- src/component/Counter.tsx
+++ src/component/Counter.tsx
@@ -1,14 +1,64 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import { Button } from "semantic-ui-react";
+import { createStore, Reducer } from "redux";
+import { useSelector, useDispatch } from "react-redux";
+
+// actions
+const CounterActionType = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT"
+} as const;
+
+type ValueOf<T> = T[keyof T];
+
+type CounterAction = {
+  type: ValueOf<typeof CounterActionType>;
+};
+
+const increment = (): CounterAction => ({
+  type: CounterActionType.INCREMENT
+});
+
+const decrement = (): CounterAction => ({
+  type: CounterActionType.DECREMENT
+});
+
+// reducer
+type CounterState = {
+  count: number;
+};
+const counterReducer: Reducer<CounterState, CounterAction> = (
+  state = { count: 0 },
+  action: CounterAction
+) => {
+  switch (action.type) {
+    case CounterActionType.INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    case CounterActionType.DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+};
+
+// store
+export const store = createStore(counterReducer, { count: 0 });
 
 const Counter: FC<{}> = () => {
-  const [count, setCount] = useState(0);
+  const count = useSelector<CounterState, number>((state) => state.count);
+  const dispatch = useDispatch();
   return (
     <>
       <h1>This is Counter</h1>
       <p>{count}</p>
-      <Button onClick={() => setCount(count + 1)}>increment</Button>
-      <Button onClick={() => setCount(count - 1)}>decrement</Button>
+      <Button onClick={() => dispatch(increment())}>increment</Button>
+      <Button onClick={() => dispatch(decrement())}>decrement</Button>
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./component/Counter";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
